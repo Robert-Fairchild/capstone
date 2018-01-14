925f48fb1e5ebb189597b6196849527e350670f1
Robert Fairchild <robertjoseph.fairchild@gmail.com>
2018-01-14 16:06:54 -0600

    Added VueJS components with errors

--- public/index.js
+++ public/index.js
@@ -4,7 +4,8 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to CanIWorkHere.com!"
+      message: "Welcome to CanIWorkHere.com!",
+      posts: []
     };
   },
   created: function() {
